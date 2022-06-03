eb0da03569b9c5a427e737af7d7f2b9870a9f519
joyunju <whdbswn1113@naver.com>
2022-06-03 18:18:00 +0900

    setting

---
+++ src/com/javaex/dao/TestApp.java
@@ -0,0 +1,19 @@
+package com.javaex.dao;
+
+import com.javaex.vo.PersonVo;
+
+public class TestApp {
+
+	public static void main(String[] args) {
+
+		PhoneDao phoneDao = new PhoneDao();
+//		List<PersonVo> personList = phoneDao.getPersonList();
+//		
+//		System.out.println(personList.toString());
+
+		PersonVo personVo = phoneDao.getPerson(0);
+		System.out.println(personVo);
+
+	}
+
+}
